b211fda14a68ea3118db50d676730f617f0185fb
Rocadinis <8b21espq@gmail.com>
2023-01-11 23:30:56 +0000

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -7,25 +7,78 @@ from tkinter import messagebox
 title("f(x)Visualizer")
 screensize(canvheight=800, canvwidth=800, bg="black")
 color("white")
+speed(3000)
 ht()
-speed(8)
 goto(-800, 0)
-forward(1600)
+for x in range(10):
+    forward(70)
+    left(90)
+    forward(70)
+    right(90)
+    right(90)
+    forward(140)
+    left(90)
+    left(90)
+    forward(70)
+    right(90)
+forward(140)
+for x in range(10):
+    forward(70)
+    left(90)
+    forward(70)
+    right(90)
+    right(90)
+    forward(140)
+    left(90)
+    left(90)
+    forward(70)
+    right(90)
 penup()
 goto(0, 800)
 pendown()
 down()
 right(90)
-forward(1600)
+for x in range(10):
+    forward(70)
+    left(90)
+    forward(70)
+    right(90)
+    right(90)
+    forward(140)
+    left(90)
+    left(90)
+    forward(70)
+    right(90)
+forward(140)
+for x in range(10):
+    forward(70)
+    left(90)
+    forward(70)
+    right(90)
+    right(90)
+    forward(140)
+    left(90)
+    left(90)
+    forward(70)
+    right(90)
+left(90)
 # Info and drawing
 def checkf():
     function = textinput("Function box", "Insert a function")
-    while function != "" and function is not None:
-        pass
-        # from this part onward, the program will parse and draw the function. do later
-    if function == None:
+    if function != "" and function is not None:
+        penup()
+        color("red")
+        left(90)
+        pensize(5)
+        vals = function.split(",")
+        x = vals[0]
+        y = vals[1]
+        pendown()
+        goto (int(x) * 1000, int(y) * 1000)
+        # prolly not completely right, must fix later
+        done()
+    elif function == None:
         messagebox.showerror("Error", "Process aborted.")
-        sleep(1)
         done()
     else:
         messagebox.showwarning("Warning", "The function cannot be empty.")
